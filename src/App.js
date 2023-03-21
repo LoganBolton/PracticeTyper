0b8ac13ce48adaf52ac81bd02becb6f52f47c788
Logan Bolton <loganbolton101@gmail.com>
2023-03-20 22:46:05 -0500

    basic

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
 function App() {
+  const [text, setText] = useState("");
+  const [list, setList] = useState([]);
+  const [introText, setIntroText] = useState("Type out this string.");
+
+  let statusMessage;
+  if (text === introText) {
+    statusMessage = <p>Status matches</p>;
+  } else {
+    statusMessage = <p>You suck</p>;
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ display: "flex" }}>
+      <div style={{ flex: 1 }}>
+        <p>{introText}</p>
+        <input
+          type="text"
+          value={text}
+          onChange={(e) => setText(e.target.value)}
+          onKeyDown={(e) => {
+            if (e.key === "Enter") {
+              setList([text, ...list]);
+              setText("");
+            }
+          }}
+        />
+        <ul>
+          {list.map((item, index) => (
+            <li key={index}>{item}</li>
+          ))}
+        </ul>
+        {statusMessage}
+      </div>
+      <div style={{ flex: 1 }}>
+        <p>Current state of const text: {text}</p>
+        <p>Current state of const list: {JSON.stringify(list)}</p>
+        <p>Current state of const introText: {introText}</p>
+      </div>
     </div>
   );
 }
