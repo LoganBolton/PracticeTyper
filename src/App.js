48b7b9f852aa2e25c9f01869c1620be16b61e011
Logan Bolton <loganbolton101@gmail.com>
2023-05-08 23:28:14 -0500

    conditional formatting boiler plate

--- src/App.js
+++ src/App.js
@@ -1,21 +1,31 @@
 import React, { useState } from "react";
+import './index.css';
 
 function App() {
   const [text, setText] = useState("");
   const [list, setList] = useState([]);
   const [introText, setIntroText] = useState("Type out this string.");
+  const [isGreen, setIsGreen] = useState(false);
 
-  let statusMessage;
-  if (text === introText) {
-    statusMessage = <p>Status matches</p>;
-  } else {
-    statusMessage = <p>You suck</p>;
-  }
+  const lastCharMatch = text[text.length - 1] === introText[text.length - 1];
+  
+  const toggleColor = () => {
+    setIsGreen(!isGreen);
+  };
+
+  const statusSquare = lastCharMatch ? (
+    <div style={{ backgroundColor: "green", width: 50, height: 50 }}></div>
+  ) : (
+    <div style={{ backgroundColor: "red", width: 50, height: 50 }}></div>
+  );
 
   return (
     <div style={{ display: "flex" }}>
       <div style={{ flex: 1 }}>
-        <p>{introText}</p>
+        <div className="containerDiv">
+          <p className="baseText" >{introText}</p>
+          <p className="upperText" >{text}</p>
+        </div>
         <input
           type="text"
           value={text}
@@ -32,12 +42,18 @@ function App() {
             <li key={index}>{item}</li>
           ))}
         </ul>
-        {statusMessage}
+        {statusSquare}
       </div>
       <div style={{ flex: 1 }}>
         <p>Current state of const text: {text}</p>
         <p>Current state of const list: {JSON.stringify(list)}</p>
-        <p>Current state of const introText: {introText}</p>
+        <p>Current state of const introText:</p>
+        <p className="bold-text">This text is bold</p>
+
+      </div>
+      <div className={`my-class ${isGreen ? 'green' : 'red'}`}>
+        <button onClick={toggleColor}>Toggle color</button>
+        <p>This is my class</p>
       </div>
     </div>
   );
