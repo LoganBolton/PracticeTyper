34a107c567d5a596fd10f99c91f0f10d11df61ef
Logan Bolton <loganbolton101@gmail.com>
2023-05-15 00:23:47 -0500

    find incorrect values

--- src/App.js
+++ src/App.js
@@ -5,51 +5,43 @@ function App() {
   const [text, setText] = useState("");
   const [promptText, setPromptText] = useState("type out this string");
   const [isInputMatch, setIsInputMatch] = useState(false);
-  const [isInputEnabled, setIsInputEnabled] = useState(true);
-  const [lastCharMatch, setLastCharMatch] = useState(true);
   const [wrongChars, setWrongChars] = useState({})
-
-  let prevUserInput;
-
-  const statusSquare = lastCharMatch ? (
-    <div style={{ backgroundColor: "green", width: 50, height: 50 }}></div>
-  ) : (
-    <div style={{ backgroundColor: "red", width: 50, height: 50 }}></div>
-  ); 
 
   const handleInputChange = (e) => {
     const inputValue = e.target.value;
+    setText(inputValue);
 
-    checkLastChar(inputValue);
-    charInput(inputValue);    
-    if (inputValue == promptText) {
-      setIsInputMatch(true);
+    // Remove key-value pair from wrongChars if corresponding character is deleted
+    if (inputValue.length < text.length) {
+      const deletedIndex = inputValue.length;
+      const { [deletedIndex]: deletedChar, ...rest } = wrongChars;
+      setWrongChars(rest);
     }
   };
+  
+  const findWrongChars = () => {
+    const charArray = text.split("");
+    const newWrongChars = {};
 
-  const checkLastChar = (inputValue) => {
-    if (isInputEnabled == true) {
-      const currentStatus = inputValue[inputValue.length - 1] === promptText[inputValue.length - 1];
-      setLastCharMatch(currentStatus);
-      setIsInputEnabled(currentStatus);
-
-      if (!currentStatus) {
-        setWrongChars({ ...wrongChars, [text.length]: false });
+    charArray.forEach((char, index) => {
+      // console.log(`Character at index ${index} is ${char}`);
+      if (char !== promptText[index]) {
+        setWrongChars({ ...wrongChars, [index]: false });
       }
-    }
+      else {
+        setWrongChars({ ...wrongChars, [index]: true });
+      }
+    });
   }
-
-  const charInput = (inputValue) => {
-    // if (isInputEnabled) {
-    //   setText(inputValue);
-    // }
-    setText(inputValue);
-  }
+  
+  useEffect(() => {
+    findWrongChars();
+  }, [text]);
 
   return (
     <div style={{}}>
       <div id="wrapperHeader">
-        <div class="" id="header">
+        <div className="" id="header">
           <h1>
             TypeScripts
           </h1>
@@ -64,43 +56,23 @@ function App() {
                 wrap="soft"
                 value={text}
                 onChange={handleInputChange}
-                onKeyUp={(e) => {
-                if ((e.keyCode === 8 || e.keyCode === 46)) {
-                    let editedText = text.slice(0, -1);
-                    setText(editedText);
-                    // setLastCharMatch(true);
-                  }
-                }}
               />
             </div>
+            <p>prompt: "{promptText}"</p>
           </div>
         </div>
       <div className="debugInfo">
-        <div className="promptText">
-            {promptText.split('').map((char, index) => (
-              <p key={index} className={index === text.length - 1 && !lastCharMatch ? 'hidden' : 'correct'}>
-                {char === ' ' ? '\u00A0' : char} 
-              </p> /*allows for spaces */
-            ))}
-        </div>
-        <div className= "inputText">
-          {text.split('').map((char, index) => (
-            <p key={index} className={index === text.length - 1 && !lastCharMatch ? 'wrong' : 'correct'}>
-              {char === ' ' ? '\u00A0' : char} 
-            </p> /*allows for spaces */
-          ))}
-        </div>
-        {statusSquare}
         <p>text: "{text}"</p>
-        <p>inputEnabled: "{isInputEnabled.toString()}"</p>
-        <p>last char: {lastCharMatch.toString()}</p>
         {isInputMatch ? <p>Input matches prompt!</p> : <p>Input does not match prompt.</p>}
         <h3>Wrong Characters:</h3>
         <ul>
-          {Object.entries(wrongChars).map(([index, value]) => (
-          <p key={index}>{index}: {value.toString()}</p>
+          {Object.keys(wrongChars).map((index) => (
+            <li key={index}>
+              Index {index} is {wrongChars[index] ? "correct" : "wrong"}
+            </li>
           ))}
         </ul>
+
         {/* <p id="color-change">Move your cursor over this text to change its color.</p> */}
       </div>
     </div>
